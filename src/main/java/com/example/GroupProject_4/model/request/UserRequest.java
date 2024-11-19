4eee7de727f0d1bdf5b3b2e2b1abf878f05455f4
SabaSulakvelidze <SabaSulakvelidze@gmail.com>
2024-11-19 19:00:58 +0400

    added schemas

--- src/main/java/com/example/GroupProject_4/model/request/UserRequest.java
+++ src/main/java/com/example/GroupProject_4/model/request/UserRequest.java
@@ -14,16 +14,19 @@ public class UserRequest {
     @NotBlank(message = "firstName can not be empty")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "firstName must contain only latin letters and no spaces")
     @Size(min = 2,max = 32, message = "firstName size must be between 2-32 characters")
+    @Schema(description = "firstName of the user", example = "John Doe", minLength = 2, maxLength = 32)
     private String firstName;
 
     @NotBlank(message = "lastName can not be empty")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "lastName must contain only latin letters and no spaces")
     @Size(min = 2,max = 64, message = "lastName size must be between 2-64 characters")
+    @Schema(description = "lastName of the user", example = "Doe")
     private String lastName;
 
     @NotBlank(message = "userName can not be empty")
     @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "userName must contain only latin letters or numbers, no spaces")
     @Size(min = 2,max = 64, message = "userName size must be between 2-64 characters")
+    @Schema(description = "userName of the user", example = "JD123")
     private String userName;
 
     @NotNull(message = "birthDate can not be null")
